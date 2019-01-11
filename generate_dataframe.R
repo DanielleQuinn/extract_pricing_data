5f14eb003ea440ac305b8b1a1d9d50af96bd74f8
Danielle Quinn <danielle.quinn@acadiau.ca>
2019-01-11 19:28:46 -0400

    Add files via upload

---
+++ generate_dataframe.R
@@ -0,0 +1,46 @@
+library(rvest)
+library(dplyr)
+
+# 18 - 27 for 2018
+# 30 - 41 for 2017
+# 44 - 55 for 2016
+
+datalist = list()
+count = 0
+for(i in c(18:27, 30:41, 44:55))
+{
+  count = count + 1
+  main1 <- html("https://www.nrcan.gc.ca/energy/oil-sands/18087#cop") %>%
+    html_nodes("ul > li") %>%
+    .[[i]] %>%
+    as.character() %>%
+    substr(start = 14, stop = nchar(.)) %>%
+    substr(start = 1, stop = which(strsplit(., split = "")[[1]]==">")-2) %>%
+    html() %>%
+    html_nodes("table")
+  
+  if(length(grep("colspan", main1 %>% as.character())) == 0)
+  {
+    results <- main1 %>% html_table() %>%
+      data.frame()
+    names(results) <- gsub("([^A-Za-z0-9 ])+", ".", names(results))
+    datalist[[count]] <- results    
+  }
+  
+  if(length(grep("colspan", main1 %>% as.character())) == 1)
+  {
+    print(paste("Could not get data from", html("https://www.nrcan.gc.ca/energy/oil-sands/18087#cop") %>%
+                  html_nodes("ul > li") %>%
+                  .[[i]] %>%
+                  as.character() %>%
+                  substr(start = 14, stop = nchar(.)) %>%
+                  substr(start = 1, stop = which(strsplit(., split = "")[[1]]==">")-2),
+          "because rows span multiple columns."))
+  }
+}
+
+all_data = do.call(bind_rows, datalist) %>% dplyr::filter(!Date %in% c("Average", "-"))
+write.csv(all_data, "all_data.csv")
+
+
+
